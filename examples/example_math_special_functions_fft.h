ebd55c99a761aaf15ceb704836ce1572a012c4b6
alexey <tykvinsky.alex@inbox.ru>
2023-07-03 13:01:11 +0500

    [v1/develop] fft example restored

--- examples/example_math_special_functions_fft.h
+++ examples/example_math_special_functions_fft.h
@@ -5,95 +5,76 @@
 #include "config.h"
 #include "../include/filters_real_time_spectrum_analyzer.h"
 
-// Recursive Fourier filter
-/*
+// Fast Fourier transform example
 int fft_example()
 {
-    typedef double __type;
+    QElapsedTimer timer;
+    int N = 128;
+    int M = 80;
+    double *sine = __alloc__<double>(M);
+    Complex<double> *spectrum0 = __alloc__< Complex<double> >(N);
 
-    // emulation parameters:
-    double Fs                = 4000;
-    double Fn                = 50;
-    double time              = 0;
-    double EmulationDuration = 0.08;
-    int    CycleWidth        = 5;
-    int    cycles_num        = 1000 * EmulationDuration / CycleWidth;
-    int    frames_per_cycle  = CycleWidth * Fs / 1000;
 
-    #ifdef WRITE_LOGS
+    // logs directory:
+    std::string directory = "C:\\Qt_projects\\DigitalFilters_x32\\logs";
 
     // log files
     std::ofstream xt;
     std::ofstream yt;
-    std::ofstream re;
-    std::ofstream im;
-    std::ofstream am;
-    std::ofstream pH;
-    std::ofstream Km;
-    std::ofstream dt;
 
     // open files
-    xt.open( LOGS_DIRECTORY + OUTPUT_STREAM_INPUT);
-    yt.open( LOGS_DIRECTORY + OUTPUT_STREAM_OUTPUT);
-    pH.open( LOGS_DIRECTORY + OUTPUT_STREAM_PHASE_RESPONSE);
-    Km.open( LOGS_DIRECTORY + OUTPUT_STREAM_AMPLITUDE_RESPONSE);
-    dt.open( LOGS_DIRECTORY + OUTPUT_STREAM_TIME);
-    re .open(LOGS_DIRECTORY + OUTPUT_STREAM_REAL_COMPONENT);
-    im .open(LOGS_DIRECTORY + OUTPUT_STREAM_IMAG_COMPONENT);
-    am .open(LOGS_DIRECTORY + OUTPUT_STREAM_SIGNAL_AMPLITUDE);
+    yt .open(directory + "\\yt.txt");
+    xt .open(directory + "\\xt.txt");
 
-    #endif
 
-    // main driver code
-    generator<__type> gen;
-    digital_clock<double> time_provider;
-    time_provider.init(Fs);
-
-    quadrature_demodulator rff;
-    rff.init(Fs, Fn, 5);
-
-    for( int i = 0 ; i < cycles_num ; i++ )
+    for( int i = 0 ; i < M ; i++ )
     {
-        for( int j = 0 ; j < frames_per_cycle ; j++, time = time_provider.tick())
-        {
-            __type signal = gen.sine( 1, 2*Fn, 0, time ) + 0.2;
-            Complex<__type> output = rff.filt( &signal, 2 );
+        sine[i] = sin( 2.0 * 3.14 * 1 * 50.0 * (double)i / (M*50) );
 
-            // logginig
-            #ifdef WRITE_LOGS
-            yt << signal << "\n";
-            re << __realf__(output) << "\n";
-            im << __imagf__(output) << "\n";
-            am << __cabsf__(output) << "\n";
-            dt << time << "\n";
-            #endif
-        }
+        xt << sine[i] << "\n";
+
+        /*
+        sine[i] = 0.5 +
+                  sin( 2.0 * 3.14 * 1 * 50.0 * (double)i / (M*50) ) +
+                  sin( 2.0 * 3.14 * 2 * 50.0 * (double)i / (M*50) ) +
+                  sin( 2.0 * 3.14 * 3 * 50.0 * (double)i / (M*50) );
+                  */
     }
 
-    for( int i = 0 ; i < Fs / 2 ; i++ )
-    {
-        Complex<__type> output = rff.frequency_response( (double)i );
+    // fft
+    timer.start();
+    for( int i = 0 ; i < 1e3 ; i++ ) fft0( sine, spectrum0, M, N, 1 );
+    //for( int i = 0 ; i < 1e3 ; i++ ) fft1( sine, spectrum0, M, N, 1 );
+    double dt = timer.nsecsElapsed() / 1e9;
 
-        #ifdef WRITE_LOGS
-        pH << __cargf__(output) << "\n";
-        Km << __cabsf__(output) << "\n";
-        #endif
+    // display info
+    cout << "FFT has taken = " << dt << "s" << "\n\n";
+    cout << "Spectrum magnitudes of direct transform: \n\n";
+    for( int i = 0 ; i < N ; i++ ) cout << "S[" << i << "]" << spectrum0[i].to_string() << "\n";
+    cout << "\n";
+
+    for( int i = 0 ; i < M ; i++ )
+    {
+        sine[i] = 0;
     }
 
-    // close files
-    #ifdef WRITE_LOGS
+    //fft0( sine, spectrum0, M, N, 0, 3);
+    fft1( sine, spectrum0, M, N, 0, 3);
+
+    cout << "Spectrum magnitudes of inverse transform: \n\n";
+    for( int i = 0 ; i < M ; i++ )
+    {
+        yt << sine[i] << "\n";
+        //cout << "S[" << i << "]" << sine[i] << "\n";
+    }
+
     xt.close();
     yt.close();
-    re.close();
-    im.close();
-    am.close();
-    pH.close();
-    Km.close();
-    dt.close();
-    #endif
+
+    __mfree__(spectrum0);
+    __mfree__(sine);
 
     return 0;
 }
-*/
 
 #endif // EXAMPLE_MATH_SPECIAL_FUNCTIONS_FFT_H
