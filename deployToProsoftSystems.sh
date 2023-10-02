612d5d4af3482e9f8fb0acc787337bf7b8bf8bb6
alexey <tykvinsky.alex@inbox.ru>
2023-10-02 15:31:12 +0500

    [v1/comtrade_emulation_added] new deployment script generated

--- deployToProsoftSystems.sh
+++ deployToProsoftSystems.sh
@@ -3,20 +3,22 @@
 # project to ProsoftSystems project
 
 #retrieve source and target directories
-sourceDirectory=$"C:/Qt_projects/DSP/source"
-targetDirectory=$"C:/Qt_projects/ProsoftSystems/source"
+sourceDirectory=$"C:/Qt_projects/DSP"
+targetDirectory=$"C:/Qt_projects/ProsoftSystems"
+sourceFilesPath=$"source"
+libsFilesPath=$"libs"
 
 #save default field separators
 defaultIFS=$IFS;
 
 #check source and target directories
-if [ ! -d $sourceDirectory ]
+if [ ! -d $sourceDirectory/$sourceFilesPath ]
 then
 	echo "name $sourceDirectory is not a directory"
 	exit
 fi
 
-if [ ! -d $targetDirectory ]
+if [ ! -d $targetDirectory/$sourceFilesPath ]
 then
 	echo "name $targetDirectory is not a directory"
 	exit
@@ -30,7 +32,7 @@ then
 fi
 
 #copy relevant files from source to target directory
-for path in $targetDirectory/*
+for path in $targetDirectory/$sourceFilesPath/*
 do
 	#parse filename from target directory
 	IFS="/"
@@ -40,9 +42,17 @@ do
 	
 	#check if source directory has such a file
 	#if so, we copy it to target directory
-	if [ -f $sourceDirectory/$filename ]
+	if [ -f $sourceDirectory/$sourceFilesPath/$filename ]
 	then
 		echo "copy: $filename from $sourceDirectory to $targetDirectory"
-		cp $sourceDirectory/$filename $targetDirectory
+		cp $sourceDirectory/$sourceFilesPath/$filename $targetDirectory/$sourceFilesPath
 	fi
 done
+
+# copy libs
+echo "copy related libs"
+
+rm $targetDirectory/$libsFilesPath
+cp -r $sourceDirectory/$libsFilesPath/ $targetDirectory/$libsFilesPath
+
+read -p "Press any key to continue" x
